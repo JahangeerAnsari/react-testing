74e8066b4d96a9776f9bf350cfcf5ff4faea6b45
jansari <jansari@entiovi.com>
2022-06-04 10:14:51 +0530

    Test and button color change

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [butttonColor,setButtonColor,] = useState("red")
+  const newButtonColor = butttonColor === "red" ? 'blue':'red'
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+      <button style={{backgroundColor:butttonColor}}
+      onClick={() => setButtonColor(newButtonColor)}
+      >Change to blue</button>
+   </div>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText("Learn React");
-  expect(linkElement).toBeInTheDocument();
+test('button has initial color', () => {
+  render(<App/>);
+  // find an element with a role of button and text of 'Change to blue'
+  const colorButton = screen.getByRole('button',{name:'Change to blue'});
+  //expect the background color to be red
+  expect(colorButton).toHaveStyle({ backgroundColor:'red' })
+  // clicked button
+  fireEvent.click(colorButton);
+
+  // expect the background color should be red
+  expect(colorButton).toHaveStyle({ backgroundColor:"blue"});
+
+  //expect the button color should be changed to blue
+  expect(colorButton.textContent).toBe('Change to blue');
 });
+
+// test('button turns blue when clicked', () => {
+ 
+// });
